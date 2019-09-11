93f243cc11cb43966c9cee6d81b38c74b8f7cdaf
温明妍 <wenmy@tuya.com>
2019-09-11 18:38:39 +0800

    create group demo

---
+++ Example/TuyaSmartHomeKit/DeviceList/Controller/TYDeviceGroupViewController.h
@@ -0,0 +1,21 @@
+//
+//  TYDeviceGroupViewController.h
+//  TuyaSmartHomeKit_Example
+//
+//  Created by 温明妍 on 2019/9/11.
+//  Copyright © 2019 xuchengcheng. All rights reserved.
+//
+
+#import "TPBaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@class TuyaSmartDevice;
+
+@interface TYDeviceGroupViewController : TPBaseViewController
+
+@property (nonatomic, strong) TuyaSmartDevice *device;
+
+@end
+
+NS_ASSUME_NONNULL_END
